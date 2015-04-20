ff9b73c26c2375141125f8b2e2dc4b705863a33d
libresmartphone <immacv62@gmail.com>
2015-04-20 20:27:41 +0200

    Add kkc.py

---
+++ kkc.py
@@ -0,0 +1,23 @@
+from PyQt4 import QtGui, QtCore
+import serial
+
+ser = serial.Serial('/dev/ttyACM0', 115200)
+
+class Window(QtGui.QWidget):
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+        self.button = QtGui.QPushButton('Call', self)
+        self.button.clicked.connect(self.handleButton)
+        layout = QtGui.QVBoxLayout(self)
+        layout.addWidget(self.button)
+
+    def handleButton(self):
+        ser.write('c')
+
+if __name__ == '__main__':
+
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
